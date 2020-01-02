27d1d6e460cc64bb7593e017fe658785e944a8b4
choiyeonseok <smr603@snu.ac.kr>
2020-01-03 00:23:11 +0900

    useRef sample

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,35 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
+    const nameInput = useRef();
 
     const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
-    const onChange = (e) => {  // e => 어떤 input에서 변화가 있었는지
-        const { value, name } = e.target;
+    const onChange = e => {
+        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
         setInputs({
-            ...inputs, //기존의 input객체를 복사한 뒤
-            [name]: value
+            ...inputs, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
         });
     };
+
     const onReset = () => {
         setInputs({
             name: '',
             nickname: ''
         });
+        nameInput.current.focus();
     };
+
     return (
         <div>
-            <input name='name' placeholder="이름" onChange={onChange} value={name}/>
-            <input name='nickname' placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <input
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+                ref={nameInput}
+            />
+            <input
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {name}({nickname})
-            </div>
+                {name} ({nickname})
+      </div>
         </div>
     );
 }
