3bf448c1e3d6aa39a28c55913b6ae391c749dc93
choiyeonseok <smr603@snu.ac.kr>
2020-01-02 18:44:51 +0900

    render for props.children

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 import './App.css';
 
 
 function App() {
   
   return (
-      <>
+      <Wrapper>
         <Hello name="react" color="red"/>
         <Hello color="pink" />
-      </>
+      </Wrapper>
   );
 }
 
