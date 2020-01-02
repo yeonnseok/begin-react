34388cfb43652d3bfe925ba186665be5dbd3a4e2
choiyeonseok <smr603@snu.ac.kr>
2020-01-03 00:58:59 +0900

    Add list contents using spread operand

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  })
+
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [ users, setUsers ] = useState([ //[상태, 세터] = useState(초기값)
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+
   return (
-      <UserList/>
+      <>
+        <CreateUser 
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate}
+        />
+        <UserList users={users}/>
+      </>
   );
 }
 
