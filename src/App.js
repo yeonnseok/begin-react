f7ae752e687d822b6cafaabbf5e94ee63a6d68bb
choiyeonseok <smr603@snu.ac.kr>
2020-01-02 18:32:36 +0900

    first props

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
-
+  
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+      <Hello name="react" color="red"/>
   );
 }
 
