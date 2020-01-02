9337f4654f2844a28a0cf0d74af1607634702958
choiyeonseok <smr603@snu.ac.kr>
2020-01-03 00:35:17 +0900

    Rendering array with key-index

--- src/UserList.js
+++ src/UserList.js
@@ -30,9 +30,9 @@ function UserList() {
 
     return (
         <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {users.map((user, index) => (
+                <User user={user} key={index}/>
+            ))}
         </div>
     );
 };
