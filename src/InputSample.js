d3fd1b7574f616f3f7d163b610caeaa14a2261ac
choiyeonseok <smr603@snu.ac.kr>
2020-01-03 00:16:12 +0900

    Managing multiple inputs

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
 
-    const onChange = (e) => {
-        setText(e.target.value);
-    }
+    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
+    const onChange = (e) => {  // e => 어떤 input에서 변화가 있었는지
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs, //기존의 input객체를 복사한 뒤
+            [name]: value
+        });
+    };
     const onReset = () => {
-        setText('');
-    }
-
+        setInputs({
+            name: '',
+            nickname: ''
+        });
+    };
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input name='name' placeholder="이름" onChange={onChange} value={name}/>
+            <input name='nickname' placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name}({nickname})
             </div>
         </div>
     );
