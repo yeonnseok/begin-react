3e8ac49a2bc35db85c685105877d782a47ec5927
choiyeonseok <smr603@snu.ac.kr>
2020-01-03 23:50:56 +0900

    Create Context API

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { UserDispatch } from '.App';
 
-const User = React.memo(function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user }) {
+    const dispatch = useContext(UserDispatch);
+
     return (
         <div>
             <b
@@ -8,35 +11,34 @@ const User = React.memo(function User({ user, onRemove, onToggle }) {
                     cursor: 'pointer',
                     color: user.active ? 'green' : 'black'
                 }}
-                onClick={() => onToggle(user.id)} //인자를 넣어주어야하기 때문에 화살표함수를 사용
+                onClick={() => {
+                    dispatch({ type: 'TOGGLE_USER', id: user.id });
+                }} //인자를 넣어주어야하기 때문에 화살표함수를 사용
             >
                 {user.username}
             </b>
             &nbsp; 
             <span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
+            <button onClick={() => {
+                dispatch({ type: 'REMOVE_USER', id: user.id });
+            }}>삭제</button>
         </div>
     );
 });
 
 
-function UserList({ users, onRemove, onToggle }) {
+function UserList({ users }) {
     return (
         <div>
             {users.map(user => (
                 <User 
                     user={user} 
                     key={user.id}
-                    onRemove={onRemove} 
-                    onToggle={onToggle}
                 />
             ))}
         </div>
     );
 };
 
-export default React.memo(
-    UserList,
-    (prevProps, nextProps) => prevProps.users === nextProps.users
-);
+export default React.memo(UserList);
 
