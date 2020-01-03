bb1101f8d128cace820f9ba83c9d0f0ea5b151dc
choiyeonseok <smr603@snu.ac.kr>
2020-01-03 14:05:05 +0900

    Add useInputs custom Hooks using reducer

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -1,13 +1,28 @@
 import { useState, useCallback } from 'react';
 
+function reducer(state, action) {
+    switch(action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                [action.name]: action.value
+            };
+        case 'RESET':
+            return Objects.keys(state).reduce((acc, current) => {
+                acc[current] = '',
+                return acc;
+            }, {});
+    }
+}
+
 function useInputs(initialForm) {
     const [form, setForm] = useState(initialForm);
-
+    //change
     const onChange = useCallback(e => {
         const { name, value } = e.target;
-        setForm(form => ({...form, [name]: value}));
+        dispatch({ type: 'CHANGE', name, value });
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    const reset = useCallback(() => dispatch({ type: 'RESET' }), []);
     return [form, onChange, reset];
 }
 
